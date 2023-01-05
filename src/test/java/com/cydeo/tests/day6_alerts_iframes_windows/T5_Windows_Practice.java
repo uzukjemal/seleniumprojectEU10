ec1f10c4f3fc3ce0f293d6a4e6157e3345b54ad7
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-05 14:31:03 +0400

    day6 Window Handle

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,11 +21,51 @@ public class T5_Windows_Practice {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //Go to: https://practice.cydeo.com/iframe
-        driver.get("https://practice.cydeo.com/iframe");
+        driver.get("https://practice.cydeo.com/windows ");
+
     }
 
     @Test
     public void multiple_window_test(){
+
+        //Storing the main page's window handle as string is
+        //good practice for future re-usable purposes
+
+        String mainHandle = driver.getWindowHandle();
+
+        System.out.println("mainHandle = " + mainHandle);
+
+        //4. Assert: Title is “Windows”
+        String expectedTitle = "Windows";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+        System.out.println("Title before click:" + actualTitle);
+
+        //5. Click to: “Click Here” link
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
+
+
+
+
+        //6. Switch to new Window.
+        for(String each: driver.getWindowHandles()){
+            driver.switchTo().window(each);
+            System.out.println("Current title while switching windows: " + driver.getTitle());
+        }
+
+        //7. Assert: Title is “New Window”
+        String expectedTitleAfterClick = "New Window";
+        actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitleAfterClick);
+
+
+
+        actualTitle = driver.getTitle();
+        System.out.println("Title after click:" + actualTitle);
 
     }
 
