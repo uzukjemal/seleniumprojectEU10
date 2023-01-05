8b891ac2979845fbab1ea46c34f3d63e5346d52c
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-05 13:31:25 +0400

    day6 Window Handle

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -0,0 +1,30 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_Windows_Practice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Go to: https://practice.cydeo.com/iframe
+        driver.get("https://practice.cydeo.com/iframe");
+    }
+
+    @Test
+    public void multiple_window_test(){
+
+    }
+
+
+}
