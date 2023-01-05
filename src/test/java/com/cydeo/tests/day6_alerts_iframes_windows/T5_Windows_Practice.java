c334a57028244e66a961049bdaeeb04d6ce28d14
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-05 14:34:16 +0400

    day6 Window Handle

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -67,6 +68,11 @@ public class T5_Windows_Practice {
         actualTitle = driver.getTitle();
         System.out.println("Title after click:" + actualTitle);
 
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
     }
 
 
