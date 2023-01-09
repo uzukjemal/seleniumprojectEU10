d8f1497b2507a2bcd89f8e310ec1f5d9f2f37466
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-09 15:33:01 +0400

    day07 BrowserUtils switchWindowAndVerify

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,8 +1,53 @@
 package com.cydeo.utilities;
 
+
+/*
+In this class only general utility methods that are not related to some specific page.
+
+ */
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
 public class BrowserUtils {
 
+        /*
+        This method will accept int (int seconds) and execute Thread.sleep for given duration
+         */
 
+    public static void sleep (int second){
+        second *=1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedTitle){
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+            driver.switchTo().window(each);
+            System.out.println("Current URL: "+ driver.getCurrentUrl());
+
+            if( driver.getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+        }
+
+        //5. Assert: Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+
+
+    }
 
 
 }
