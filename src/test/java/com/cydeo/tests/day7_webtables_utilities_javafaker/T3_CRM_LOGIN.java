eeebc2601f93707114a562cca4cab2675b634f19
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-09 16:22:35 +0400

    day07 T3 CRM Login

---
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -0,0 +1,8 @@
+package com.cydeo.tests.day7_webtables_utilities_javafaker;
+
+public class T3_CRM_LOGIN {
+
+
+
+
+}
--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -17,8 +17,8 @@ public class BrowserUtils {
         This method will accept int (int seconds) and execute Thread.sleep for given duration
          */
 
-    public static void sleep (int second){
-        second *=1000;
+    public static void sleep(int second) {
+        second *= 1000;
         try {
             Thread.sleep(second);
         } catch (InterruptedException e) {
@@ -26,28 +26,56 @@ public class BrowserUtils {
         }
     }
 
-    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedTitle){
+
+
+
+    /*
+    This method accepts 3 arguments.
+    Arg1: webDriver
+    Arg2: expectedInUrl : for verify if the url contains given String.
+        - If condition matches, will break loop.
+    Arg3: expectedInTitle to be compared against actualTitle
+     */
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedTitle) {
 
         Set<String> allWindowsHandles = driver.getWindowHandles();
 
         for (String each : allWindowsHandles) {
             driver.switchTo().window(each);
-            System.out.println("Current URL: "+ driver.getCurrentUrl());
+            System.out.println("Current URL: " + driver.getCurrentUrl());
 
-            if( driver.getCurrentUrl().contains(expectedInUrl)){
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
                 break;
             }
         }
 
         //5. Assert: Title contains “Etsy”
         String actualTitle = driver.getTitle();
-
-
         Assert.assertTrue(actualTitle.contains(expectedTitle));
-
-
-
     }
+
+
+    /*
+    This method accepts a String "expectedTitle" and Asserts if it is true
+     */
+
+    public static void verifyTitle(WebDriver driver, String expectedTitle){
+
+    Assert.assertEquals(driver.getTitle(), expectedTitle);
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
