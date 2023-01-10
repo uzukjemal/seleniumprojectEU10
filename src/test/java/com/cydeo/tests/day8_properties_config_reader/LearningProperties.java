fa7a9b52d9e89ff560ede0fcfc9ff8f52b1177d0
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-10 16:07:46 +0400

    day08 Learning Properties

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
@@ -0,0 +1,17 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import org.testng.annotations.Test;
+
+public class LearningProperties {
+
+
+    @Test
+    public void java_properties_reading_test(){
+
+        System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+
+
+    }
+
+
+}
