e5ff657b1c47c96f2cd8e49fec596800002d7a44
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-19 17:41:19 +0400

    day9 JavaFaker

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -1,6 +1,53 @@
 package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
 
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+
 public class JavaFakerPractice {
 
 
-}
+
+
+    @Test
+    public void test(){
+
+        //Creating Faker object to reach methods
+        Faker faker = new Faker();
+
+
+        System.out.println("faker.name().firstName() = " + faker.name().firstName());
+        System.out.println("faker.name().lastName() = " + faker.name().lastName());
+        System.out.println("faker.name().fullName() = " + faker.name().fullName());
+
+        System.out.println("=======================================================");
+
+        // numerify() method will generate random numbers in the format we want to get
+        System.out.println("faker.numerify(\"###-###-####\") = " + faker.numerify("###-###-####"));
+
+        System.out.println("faker.numerify(\"312-###-####\") = " + faker.numerify("312-###-####"));
+
+
+        System.out.println("========================================================");
+
+
+        //letterify() method will return random letters where we pass "?"
+        System.out.println("faker.letterify(\"???-????\") = " + faker.letterify("???-????"));
+
+
+        System.out.println("faker.bothify(\"##?#-##?#-#?#?#-##??\") = " + faker.bothify("##?#-##?#-#?#?#-##??"));
+
+        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard());
+
+        System.out.println("faker.finance().creditCard() = " + faker.finance().creditCard().replace("-", ""));
+
+        System.out.println("faker.chuckNorris().fact() = " + faker.chuckNorris().fact());
+
+        System.out.println("faker.chuckNorris().fact() = "
+                + faker.chuckNorris().fact().replaceAll("Chuck Norris", "Muhtar"));
+
+
+    }
+    }
+
+
+
