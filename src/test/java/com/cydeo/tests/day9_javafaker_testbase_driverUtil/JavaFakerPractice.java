3e723229793568cbd65488fb0c06247229c8c6f0
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-19 16:56:44 +0400

    day9 JavaFaker

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+import org.testng.annotations.Test;
+
+public @interface JavaFakerPractice {
+
+
+    @Test
+    public void test1(){
+
+    }
+}
