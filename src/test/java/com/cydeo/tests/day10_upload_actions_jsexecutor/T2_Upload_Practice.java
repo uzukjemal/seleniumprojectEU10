1a0101bce28d4b0be04bfddd02d26962304546b4
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-23 00:58:26 +0400

    day19 Upload Practice

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
@@ -1,5 +1,42 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class T2_Upload_Practice {
+
+
+    @Test
+    public void upload_test(){
+
+        //1. Go to https://practice.cydeo.com/upload
+
+        Driver.getDriver().get("https://practice.cydeo.com/upload");
+
+        //2. Find some small file from your computer, and get the path of it.
+        String path = "/Users/user/Desktop/HTML Class/happyDog.jpeg";
+
+        //3. Upload the file.
+        WebElement fileUpload = Driver.getDriver().findElement(By.id("file-upload"));
+        fileUpload.sendKeys(path);
+
+        //4. Assert:
+        WebElement uploadButton = Driver.getDriver().findElement(By.id("file-submit"));
+        uploadButton.click();
+
+        //-File uploaded text is displayed on the page
+        WebElement fileUploadedHeader = Driver.getDriver().findElement(By.tagName("h3"));
+
+        Assert.assertTrue(fileUploadedHeader.isDisplayed());
+
+
+
+    }
+
+    //TC #2 Upload Test
+
 
 }
