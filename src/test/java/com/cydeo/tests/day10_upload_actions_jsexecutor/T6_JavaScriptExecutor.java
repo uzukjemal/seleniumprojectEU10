4d488cd7f868c9cf029a125399a62fb9b10a890f
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-23 01:40:11 +0400

    day19 Javascript Executor

---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavaScriptExecutor.java
@@ -0,0 +1,23 @@
+package com.cydeo.tests.day10_upload_actions_jsexecutor;
+
+import org.testng.annotations.Test;
+
+public class T6_JavaScriptExecutor {
+
+    @Test
+    public void javascript_executor_test1(){
+
+
+        //TC #6: Scroll using JavascriptExecutor
+        //1- Open a Chrome browser
+        //2- Go to: https://practice.cydeo.com/infinite_scroll
+        //3- Use below JavaScript method and scroll
+        //a.  750 pixels down 10 times.
+        //b.  750 pixels up 10 times
+        //
+        //JavaScript method to use : window.scrollBy(0,0)
+
+    }
+
+
+}
