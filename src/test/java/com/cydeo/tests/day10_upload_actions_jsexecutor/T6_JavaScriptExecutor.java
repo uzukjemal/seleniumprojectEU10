4745843890df001ec04ec6393bc1b3282b9e9f7b
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-01-23 16:06:27 +0400

    day19 Javascript Executor

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavaScriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavaScriptExecutor.java
@@ -1,5 +1,8 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 public class T6_JavaScriptExecutor {
@@ -11,11 +14,29 @@ public class T6_JavaScriptExecutor {
         //TC #6: Scroll using JavascriptExecutor
         //1- Open a Chrome browser
         //2- Go to: https://practice.cydeo.com/infinite_scroll
+        Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll");
+
+        //((JavascriptExecutor)Driver.getDriver()).executeScript("window.scrollBy(0,0)");
+
         //3- Use below JavaScript method and scroll
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+
         //a.  750 pixels down 10 times.
+
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(2);
+            js.executeScript("window.scrollBy(0,750)");
+        }
+
         //b.  750 pixels up 10 times
-        //
-        //JavaScript method to use : window.scrollBy(0,0)
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(2);
+            js.executeScript("window.scrollBy(0,-750)");
+        }
+
+        //Javascript method to use : window.scrollBy(0,0)
 
     }
 
