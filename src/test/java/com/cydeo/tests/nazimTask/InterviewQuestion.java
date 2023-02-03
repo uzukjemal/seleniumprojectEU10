11b8fec05eb76440e6ec8f77b61b489aec2c338b
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-02-03 12:55:08 +0400

    Task Iphone13

--- src/test/java/com/cydeo/tests/nazimTask/InterviewQuestion.java
+++ src/test/java/com/cydeo/tests/nazimTask/InterviewQuestion.java
@@ -16,29 +16,29 @@ public class InterviewQuestion {
         //1. Go to 'https://www.amazon.com.tr/'
         Driver.getDriver().get("https://www.amazon.com.tr/");
 
-        //to accept cookies warning
+        //2. to accept cookies warning
         WebElement acceptCookies = Driver.getDriver().findElement(By.xpath("//input[@id='sp-cc-accept']"));
         acceptCookies.click();
 
-        // Search iPhone13 512
+        //3. Search iPhone13 512
         WebElement searchBox = Driver.getDriver().findElement(By.id("twotabsearchtextbox"));
         searchBox.sendKeys("iPhone13 512"+ Keys.ENTER);
 
 
-        //3. Check that the results are listed, checking for expected/actual results
+        //4. Check that the results are listed, checking for expected/actual results
         String expectedResult = "Amazon.com.tr : iPhone13 512";
         String actualResult = Driver.getDriver().getTitle();
 
         Assert.assertEquals(actualResult, expectedResult, "Actual result text is not as expected!!!");
 
-        //4. Click iPhone13 at the top of the list
+        //5. Click iPhone13 at the top of the list
         WebElement clickIphone13AtTheTop = Driver.getDriver().findElement(By.xpath("//span[@class='a-size-base-plus a-color-base a-text-normal']"));
         clickIphone13AtTheTop.click();
 
-        //.Size information .Price .Color .Stock status
 
+        //6. Size information .Price .Color .Stock status
         WebElement itemSize = Driver.getDriver().findElement(By.xpath("(//div/p[@class='a-text-left a-size-base'])[3]"));
-        WebElement itemPrice = Driver.getDriver().findElement(By.xpath("//span[@class='a-offscreen']"));
+        WebElement itemPrice = Driver.getDriver().findElement(By.xpath("//span[@class='a-price-whole']"));
         WebElement itemColor = Driver.getDriver().findElement(By.xpath("//span[@class='selection']"));
         WebElement itemStockStatus = Driver.getDriver().findElement(By.xpath("//div/span[@class='a-size-medium a-color-success']"));
 
@@ -48,17 +48,14 @@ public class InterviewQuestion {
         System.out.println("Stock:"+ itemStockStatus.getText());
 
 
-        Driver.closeDriver();
+       Driver.closeDriver();
     }
 
 
 }
 
 
-//We expect you to automate the following steps via Selenium Web Driver, regardless of
-//. We prefer Chrome as the browser, but the choice
-//is yours.
-
-
+//We expect you to automate the following steps via Selenium Web Driver.
+//We prefer Chrome as the browser, but the choice is yours.
 //5. Log the following values for each size
 
