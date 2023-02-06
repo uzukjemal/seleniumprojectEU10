130de5b672ff8beff2860a38d6096c0a2c997c91
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-02-06 11:49:43 +0400

    day12 pom design explicit wait

---
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -0,0 +1,22 @@
+package com.cydeo.tests.day12_pom_design_explicit_wait;
+
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class POMPractices {
+
+    @Test
+    public void required_field_error_message_test(){
+
+        //1- Open a Chrome browser
+        //2- Go to: https://library1.cydeo.com
+        Driver.getDriver().get("https://library1.cydeo.com");
+
+        //3- Do not enter any information
+        //4- Click to “Sign in” button
+        //5- Verify expected error is displayed:
+        //Expected: This field is required.
+
+    }
+
+}
