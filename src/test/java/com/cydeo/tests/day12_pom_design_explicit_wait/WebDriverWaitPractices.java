7d7242d07c680b90815b3b22efe9055f46b20ffb
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-02-08 23:22:08 +0400

    day12 pom practice2

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriverWaitPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriverWaitPractices.java
@@ -1,9 +1,14 @@
 package com.cydeo.tests.day12_pom_design_explicit_wait;
 
+import com.cydeo.pages.DynamicLoad1Page;
 import com.cydeo.pages.DynamicLoad7Page;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class WebDriverWaitPractices {
 
@@ -16,11 +21,11 @@ public class WebDriverWaitPractices {
         //2. Wait until title is “Dynamic title”
 
         //creating the wait object to be able to create certain condition to wait
-        //WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
 
 
         //use the "wait" object to create our expected condition
-        //wait.until(ExpectedConditions.titleIs("Dynamic title"));
+        wait.until(ExpectedConditions.titleIs("Dynamic title"));
 
 
         //Create object of DynamicLoad7Page to reach and assert web elements
@@ -39,15 +44,29 @@ public class WebDriverWaitPractices {
 
         //1. Go to https://practice.cydeo.com/dynamic_loading/1
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
+        DynamicLoad1Page dynamicLoad1Page = new DynamicLoad1Page();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
 
         //2. Click to start
+        dynamicLoad1Page.startButton.click();
 
         //3. Wait until loading bar disappears
+        wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loading));
+
         //4. Assert username inputbox is displayed
+        Assert.assertTrue(dynamicLoad1Page.inputUsername.isDisplayed());
+
         //5. Enter username: tomsmith
+        dynamicLoad1Page.inputUsername.sendKeys("tomsmith");
+
         //6. Enter password: incorrectpassword
+        dynamicLoad1Page.inputPassword.sendKeys("SuperSecretPassword");
+
         //7. Click to Submit button
+        dynamicLoad1Page.submitButton.click();
+
         //8. Assert “Your password is invalid!” text is displayed.
+        Assert.assertTrue(dynamicLoad1Page.errorMessage.isDisplayed());
 
     }
 }
