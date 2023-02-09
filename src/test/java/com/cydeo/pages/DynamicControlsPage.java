25635be3f4b47a7d01adfe7ec71bdab15f720783
Uzukjemal <uzukjemalhudaguly@gmail.com>
2023-02-09 18:51:38 +0400

    day13 Explicit Wait Practice

--- src/test/java/com/cydeo/pages/DynamicControlsPage.java
+++ src/test/java/com/cydeo/pages/DynamicControlsPage.java
@@ -1,6 +1,7 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -11,7 +12,18 @@ public class DynamicControlsPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy
-    public void
+    @FindBy(css = "form#checkbox-example > button")
+    public WebElement removeButton;
+
+    @FindBy(css = "div#loading")
+    public WebElement loadingBar;
+
+    //@FindBy(css = "input[label='blah']")
+    @FindBy(css = "input[type='checkbox']")
+    public WebElement checkbox;
+
+    @FindBy(css = "p[id='message']")
+    public WebElement message;
+
 
 }
